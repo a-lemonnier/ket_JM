df233ef001c2de25497d34cf83a4b6e81f0bae68
some_stellar_tools <40183590+a-lemonnier@users.noreply.github.com>
2018-07-03 23:59:15 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,29 @@
 
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    
+    std::cout << "Ongoing..." << std::endl;
+    
+    _ket_JM ket_JM(_frac<>(1,1),_frac<>(1,2));
+    
+    ket_JM.compute_ket_list();
+    
+    
+//     _frac<> A=_frac<>(5,2);
+//     A.show();
+//     std::cout << "\n";
+//     _frac<> B=_frac<>(127,3);
+//     B.show();
+//     std::cout << "\n";
+//     _frac<> C=A+B;
+//     
+//     if (A<B) std::cout << "A<B\n";
+//     if (A>B) std::cout << "A>B\n";
+//     
+//     C.show();
+//     std::cout << "\n";
+//     
+    
+    
     return 0;
 }
