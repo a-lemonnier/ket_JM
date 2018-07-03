776455b6857f1211ede11c7bc1fed8afc4a669e4
some_stellar_tools <40183590+a-lemonnier@users.noreply.github.com>
2018-07-03 22:43:49 +0200

    _ket_JM::_ket_JM() filling ongoing

--- include/ket_JM.cpp
+++ include/ket_JM.cpp
@@ -1,14 +1,45 @@
 #include "ket_JM.hpp"
 
 
-_ket::_ket() {
-
+_ket_JM::_ket_JM(int j1, int j2) {
+    _frac<> A(1,1);
+    _frac<> J(1,1);
+    _frac<> M(1,1);
+    
+    JM.push_back(std::make_tuple(A, J, M));
+    
+    this->j1=_frac<>(j1, 1);
+    this->j2=_frac<>(j2, 1);
 }
 
-_ket& _ket::operator= ( const _ket &ket ) {
-    if (&ket!=this) {
-        
-        
+_ket_JM::_ket_JM(double long j1, double long j2) {
+    _frac<> A(1,1);
+    _frac<> J(1,1);
+    _frac<> M(1,1);
+    
+    JM.push_back(std::make_tuple(A, J, M));
+    
+    this->j1=_frac<>(j1, 1);
+    this->j2=_frac<>(j2, 1);
+}
+
+_ket_JM::_ket_JM(_frac<> j1, _frac<> j2) {
+    
+    _frac<> A(1,1);
+    _frac<> J(1,1);
+    _frac<> M(1,1);
+    
+    JM.push_back(std::make_tuple(A, J, M));
+    
+    this->j1=j1;
+    this->j2=j2;
+}
+
+_ket_JM& _ket_JM::operator= ( const _ket_JM &ket_JM ) {
+    if (&ket_JM!=this) {
+        this->JM=ket_JM.JM;
+        this->j1=ket_JM.j1;
+        this->j2=ket_JM.j2;
     }
     
     return *this;
--- include/ket_JM.hpp
+++ include/ket_JM.hpp
@@ -3,13 +3,30 @@
 
 #include "basic_fracs.hpp"
 
+#include<iostream>
+#include<vector>
+#include<tuple>
+#include<cmath>
 
-class _ket : public _frac<> {
+
+class _ket_JM : public _frac<> {
 public:
 
-    _ket();
+    _ket_JM(int, int);
+    _ket_JM(double long, double long);
+    _ket_JM(_frac<>, _frac<>);
+    
+    //
+    // |JM> = a1 |m11 m21 >+a2 | m12 m22 >+...
+    //
+    std::vector<std::tuple<_frac<>, _frac<>, _frac<> >> JM;
+    
+    _frac<> j1;
+    _frac<> j2;
 
-    _ket& operator= ( const _ket &ket );
+    _ket_JM& operator= ( const _ket_JM &ket );
+    
+    _ket_JM& operator* ( const _ket_JM &ket ); // Moment composition
 
 };
 
