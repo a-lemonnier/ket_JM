36afe5493968739d8c8e66d0ee56f3231c964619
some_stellar_tools <40183590+a-lemonnier@users.noreply.github.com>
2018-07-04 16:09:29 +0200

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
+#include <string>
+
 
 #include "include/basic_fracs.hpp"
 #include "include/ket_JM.hpp"
+#include "include/nuc_spin.hpp"
 
 
 int main(int argc, char **argv) {
     
-    std::cout << "Ongoing..." << std::endl;
-    
-    _ket_JM ket_JM(_frac<>(1,1),_frac<>(1,2));
+
+	_frac<> j1, j2;
+
+	std::string sj1=(argv[1]);
+	std::string sj2=(argv[2]);
+
+	std::size_t pos1=sj1.find('/');
+	std::size_t pos2=sj2.find('/');
+	
+
+	if (pos1!=std::string::npos) 
+		j1=_frac<>(std::stold(sj1.substr(0,pos1)),std::stold(sj1.substr(pos1+1,sj1.length()-1)));
+	else {
+		j1=_frac<>(std::stold(sj1),1);
+	}
+	if (pos2!=std::string::npos) 
+		j2=_frac<>(std::stold(sj2.substr(0,pos2)),std::stold(sj2.substr(pos2+1,sj2.length()-1)));
+	
+	else {
+		j2=_frac<>(std::stold(sj2),1);
+	}
+
+	
+
+	
+   
+
+    _ket_JM ket_JM(j1,j2);
     
     ket_JM.compute_ket_list();
+
+    ket_JM.show_ket_list();
+   
+    ket_JM.show_J_list(); 
+
+
+
+
     
     
-//     _frac<> A=_frac<>(5,2);
-//     A.show();
-//     std::cout << "\n";
-//     _frac<> B=_frac<>(127,3);
-//     B.show();
-//     std::cout << "\n";
-//     _frac<> C=A+B;
-//     
-//     if (A<B) std::cout << "A<B\n";
-//     if (A>B) std::cout << "A>B\n";
-//     
-//     C.show();
-//     std::cout << "\n";
-//     
+
     
     
     return 0;
